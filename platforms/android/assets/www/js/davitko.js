cfdf0f5eef58979758d7f73f8483cf7f03529be1
Miloš Slađan Davitković <davitkooo@gmail.com>
2015-04-03 23:42:43 +0200

    add search in html on top

---
+++ platforms/android/assets/www/js/davitko.js
@@ -0,0 +1,47 @@
+//$(".search-advance").hide();
+//$(document).ready(function () {
+//	$(".advance-search-open").click(function () {
+//		$(".search-advance").slideToggle();
+//	});
+//});
+
+//$(document).ready(function () {
+//
+//	$(".search-advance").hide();
+//	$(".advance-search-open").show();
+//
+//	$('.advance-search-open').toggle(function () $(".search-advance").slideDown();
+//
+//		},
+//		function () {
+//			$(".search-advance").slideUp();
+//		});
+//
+//});
+
+
+//$(function () { // DOM READY shorthand
+//
+//	$(".search-advance").hide();
+//
+//	$('.advance-search-open').click(function (e) {
+//		// e.preventDefault(); // If you use anchors
+//		var SH = this.SH ^= 1; // "Simple toggler"
+//		$(this).text(SH ? 'Hide' : 'Show')
+//			.css({
+//				backgroundPosition: '0 ' + (SH ? -18 : 0) + 'px'
+//			})
+//			.next(".search-advance").slideToggle();
+//	});
+//
+//});
+
+$(document).ready(function () {
+	$(".advance-search-open").click(function () {
+		$(".search-advance").hide();
+	});
+	debugger
+	$(".advance-search-open").click(function () {
+		$(".search-advance").show();
+	});
+});

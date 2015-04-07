9e7082c041505296262824e5a00c5f208adbd977
Miloš Slađan Davitković <davitkooo@gmail.com>
2015-04-07 18:44:15 +0200

    Add slide transitions on menu links, Add swapleft and swapright on evry page
    Unexpected shutdown of application on change screen, with menu link or
    with spaw the result is same - application crash...

--- platforms/android/assets/www/js/davitko.js
+++ platforms/android/assets/www/js/davitko.js
@@ -1,3 +1,80 @@
+// Swipe Functions
+// Map - Index Page
+$(document).on("pagecreate", "#index", function () {
+	$(".ui-content").on("swipeleft", function () {
+		$.mobile.changePage("./details.html", 'slide');
+	});
+});
+$(document).on("pagecreate", "#index", function () {
+	$(".ui-content").on("swiperight", function () {
+		$.mobile.changePage("./favorites.html", 'slide');
+	});
+});
+// Details Page
+$(document).on("pagecreate", "#details", function () {
+	$(".ui-content").on("swipeleft", function () {
+		$.mobile.changePage("./favorites.html", 'slide');
+	});
+});
+$(document).on("pagecreate", "#details", function () {
+	$(".ui-content").on("swiperight", function () {
+		$.mobile.changePage("./index.html", 'slide');
+	});
+});
+// Favourites Page
+$(document).on("pagecreate", "#favourite", function () {
+	$(".ui-content").on("swipeleft", function () {
+		$.mobile.changePage("./index.html", 'slide');
+	});
+});
+$(document).on("pagecreate", "#favourite", function () {
+	$(".ui-content").on("swiperight", function () {
+		$.mobile.changePage("./details.html", 'slide');
+	});
+});
+// Add New Kafana
+$(document).on("pagecreate", "#add_kafana", function () {
+	$(".ui-content").on("swipeleft", function () {
+		$.mobile.changePage("./index.html", 'slide');
+	});
+});
+$(document).on("pagecreate", "#add_kafana", function () {
+	$(".ui-content").on("swiperight", function () {
+		$.mobile.changePage("./index.html", 'slide');
+	});
+});
+
+
+
+
+
+
+
+//$('#index').live("swipeleft", function () {
+//	var nextpage = $('#details');
+//	// swipe using id of next page if exists
+//	alert("Levo");
+//	if (nextpage.length > 0) {
+//		$.mobile.changePage(nextpage, 'slide');
+//	}
+//});
+//$('#index').live("swiperight", function () {
+//	var prevpage = $('#details');
+//	alert("Levo");
+//	// swipe using id of next page if exists
+//	if (prevpage.length > 0) {
+//		$.mobile.changePage(prevpage, 'slide', true);
+//	}
+//});
+
+
+
+
+
+
+
+
+
 //$(".search-advance").hide();
 //$(".advance-search-open").show();
 //$(document).ready(function () {
@@ -97,4 +174,22 @@ $(document).ready(function () {
 	//
 	//	});
 
+});
+
+
+
+
+$('div.ui-page').live("swipeleft", function () {
+	var nextpage = $(this).next('div[data-role="page"]');
+	// swipe using id of next page if exists
+	if (nextpage.length > 0) {
+		$.mobile.changePage(nextpage, 'slide');
+	}
+});
+$('div.ui-page').live("swiperight", function () {
+	var prevpage = $(this).prev('div[data-role="page"]');
+	// swipe using id of next page if exists
+	if (prevpage.length > 0) {
+		$.mobile.changePage(prevpage, 'slide', true);
+	}
 });

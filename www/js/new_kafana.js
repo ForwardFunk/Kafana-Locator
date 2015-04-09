20bb728263afb36fd7227d5ec9a75d9e95dd8fbe
Pavle Đorđević <paja.92@gmail.com>
2015-04-09 12:57:29 +0200

    new_kafana changed

--- www/js/new_kafana.js
+++ www/js/new_kafana.js
@@ -5,15 +5,14 @@ document.addEventListener("deviceready", function() {
 
     var lat = unescape(tmpLat[1]);
     var lng = unescape(tmpLng[1]);
-
+    alert(lat);
+    alert(lng);
 
     document.getElementById("lat").value = lat;
     document.getElementById("lng").value = lng;
 
 
     document.getElementById("kafana_add").addEventListener("click", onAddKafana);
-
-
 
 
 }, false);
@@ -23,11 +22,35 @@ function onAddKafana() {
     var name = document.getElementById("naziv").value;
     var phone = document.getElementById("telefon").value;
     var address = document.getElementById("adresa").value;
-    var music = document.getElementById("muzika").value;
+    var music = document.getElementById("muzika").value == "on" ? "1" : "0";
     var rv_from = document.getElementById("rv_od").value;
     var rv_to = document.getElementById("rv_do").value;
+    var lat = document.getElementById("lat").value;
+    var lng = document.getElementById("lng").value;
 
-    alert(name + phone + address + music + rv_from + rv_to);
+
+
+    if (window.XMLHttpRequest) {
+        xmlHttp = new XMLHttpRequest();
+    } else {
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            alert(xmlHttp.responseText);/*
+            kafana = obj.kafane[0].kafana;
+
+            name = kafana.Naziv;
+            document.getElementById("naziv").innerHTML = kafana.Naziv;
+            document.getElementById("adresa").innerHTML = kafana.Adresa;
+            document.getElementById("telefon").innerHTML = kafana.Telefon;*/
+        }
+    }
+
+
+    xmlHttp.open("GET", "http://92.60.224.52/~fcfreek1/cgi-bin/dodajKafanu.php?naziv=" + name + "&adresa=" + address + "&telefon=" + phone
+                 + "&rv_od=" + rv_from + "&rv_do=" + rv_to + "&muzika=" + music + "&lat=" + lat + "&lng=" + lng, true);
+    xmlHttp.send();
 
 }
-
